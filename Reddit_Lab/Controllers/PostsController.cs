7f98ba58fe4cf28ff32e398bd888aedc0032b579
Tee Link <tesuanseylink@gmail.com>
2016-04-15 15:10:35 -0500

    details page linked

--- Reddit_Lab/Controllers/PostsController.cs
+++ Reddit_Lab/Controllers/PostsController.cs
@@ -23,7 +23,29 @@ namespace Reddit_Lab.Controllers
             Post post = db.Posts.Find(Id);
             return View(post);
         }
+        [HttpGet]
 
-        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Post newPost)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(newPost);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(newPost);
+            }
+        }
+
     }
 }
--- Reddit_Lab/Migrations/Configuration.cs
+++ Reddit_Lab/Migrations/Configuration.cs
@@ -17,18 +17,19 @@ namespace Reddit_Lab.Migrations
 
         protected override void Seed(Reddit_LabDbContext context)
         {
-
-            var posts = Builder<Post>.CreateListOfSize(20).All()
-                .With(p => p.UpVote = Faker.NumberFaker.Number(0, 100))
-                .With(p => p.DownVote = Faker.NumberFaker.Number(0, 100))
-                .With(p => p.Title = Faker.TextFaker.Sentences(1))
-                .With(p => p.Author = Faker.NameFaker.FirstName())
-                .With(p => p.Posted = Faker.DateTimeFaker.DateTime())
-                .With(p => p.Link = Faker.InternetFaker.Url())
-                .With(p => p.Body = Faker.TextFaker.Sentences(12))
-                .Build();
-            context.Posts.AddRange(posts);
-
+            if (!context.Posts.Any())
+            {
+                var posts = Builder<Post>.CreateListOfSize(20).All()
+                    .With(p => p.UpVote = Faker.NumberFaker.Number(0, 100))
+                    .With(p => p.DownVote = Faker.NumberFaker.Number(0, 100))
+                    .With(p => p.Title = Faker.TextFaker.Sentences(1))
+                    .With(p => p.Author = Faker.NameFaker.FirstName())
+                    .With(p => p.Posted = DateTime.Now)
+                    .With(p => p.Link = Faker.InternetFaker.Url())
+                    .With(p => p.Body = Faker.TextFaker.Sentences(12))
+                    .Build();
+                context.Posts.AddRange(posts);
+            }
 
         }
 
